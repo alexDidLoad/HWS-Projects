55f45a1f42df0e14df9bd0089625ae32c4341577
Alexander Ha <Ha.alex.h@gmail.com>
2020-11-01 18:12:39 -0800

    initial commit

---
+++ Project22/Project22/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Project22
+//
+//  Created by Alexander Ha on 11/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var distanceReading: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
