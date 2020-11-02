e8c957e01d4426ccfadacf4d3f7a12f6823ee2e6
Alexander Ha <Ha.alex.h@gmail.com>
2020-11-01 18:31:30 -0800

    add location userAuth

--- Project22/Project22/ViewController.swift
+++ Project22/Project22/ViewController.swift
@@ -6,16 +6,43 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet var distanceReading: UILabel!
+    var locationManager: CLLocationManager?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        
+        locationManager?.requestAlwaysAuthorization()
+        
+        view.backgroundColor = .black
+        
     }
 
-
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        //if we get authorized, continue...
+        if status == .authorizedAlways {
+            
+            // if our device is able to moinitor iBeacons, continue...
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                
+                //if ranging is available continue...
+                if CLLocationManager.isRangingAvailable() {
+                    
+                    //do stuff here
+                }
+            }
+        }
+    }
+    
+    
 }
 
