0fa7c132cbe086998bbe3991b83c7b9320807b6c
Alexander Ha <Ha.alex.h@gmail.com>
2020-11-04 19:33:41 -0800

    Add background and empty methods

--- Project23/Project23/GameScene.swift
+++ Project23/Project23/GameScene.swift
@@ -13,8 +13,38 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.zPosition = -1
+        background.blendMode = .replace
+        addChild(background)
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        physicsWorld.speed = 0.85
+        
+        createScore()
+        createLives()
+        createSlices()
+        
        
     }
     
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    
+    func createScore() {
+        
+    }
+    
+    func createLives() {
+        
+    }
+    
+    func createSlices() {
+        
+    }
   
 }
