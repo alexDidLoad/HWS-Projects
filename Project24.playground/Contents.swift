70b1cd2e95f8b36dd5636c12280294bcf26081b5
Alexander Ha <Ha.alex.h@gmail.com>
2020-11-10 00:04:01 -0800

    testing .hasPrefix() and .hasSuffix()

--- Project24.playground/Contents.swift
+++ Project24.playground/Contents.swift
@@ -1,3 +1,40 @@
 import UIKit
 
-var str = "Hello, playground"
+//MARK: - Strings are not Arrays
+
+let name = "Taylor"
+//we can loop through them, similar to arrays.
+//for letter in name {
+//    print(letter)
+//}
+
+//but we cannot read individual letters from the string i.e.
+//print(name[3])
+
+//reading the fourth letter in the string
+let letter = name[name.index(name.startIndex, offsetBy: 3)]
+//prints l
+print(letter)
+
+
+
+//extension that allows us to access individual letters like an array
+extension String {
+    subscript(i: Int) -> String {
+        return String(self[index(startIndex, offsetBy: i)])
+    }
+}
+//prints out 'y' in "Taylor"
+print(name[2])
+
+//MARK: - Working with Strings in Swift
+
+/* Methods for checking whether a string starts with or ends with a substring
+ - hasPrefix()
+ - hasSuffix()
+ */
+
+let password = "12345"
+password.hasPrefix("123") //returns true
+password.hasSuffix("345") //returns true
+
