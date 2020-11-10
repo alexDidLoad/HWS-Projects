b0c4d20e282438c529caf8be3db8bf81fc16a094
Alexander Ha <Ha.alex.h@gmail.com>
2020-11-10 01:54:42 -0800

    add NSAttributedString section

--- Project24.playground/Contents.swift
+++ Project24.playground/Contents.swift
@@ -101,4 +101,24 @@ swiftString.containsAny(of: languageArray) //true
 languageArray.contains(where: swiftString.contains)
 
 
-//MARK: - <#Section Heading#>
+//MARK: - Formatting strings with NSAttributedString
+
+let string = "This is a test string."
+let attributes: [NSAttributedString.Key: Any] = [
+    .foregroundColor: UIColor.white,
+    .backgroundColor: UIColor.red,
+    .font: UIFont.boldSystemFont(ofSize: 36)
+]
+
+let attributedString = NSAttributedString(string: string, attributes: attributes)
+
+
+let attributedString2 = NSMutableAttributedString(string: string)
+
+attributedString2.addAttribute(.font, value: UIFont.systemFont(ofSize: 8), range: NSRange(location: 0, length: 4))
+attributedString2.addAttribute(.font, value: UIFont.systemFont(ofSize: 16), range: NSRange(location: 5, length: 2))
+attributedString2.addAttribute(.font, value: UIFont.systemFont(ofSize: 24), range: NSRange(location: 8, length: 1))
+attributedString2.addAttribute(.font, value: UIFont.systemFont(ofSize: 32), range: NSRange(location: 10, length: 4))
+attributedString2.addAttribute(.font, value: UIFont.systemFont(ofSize: 40), range: NSRange(location: 15, length: 6))
+
+
